7460a108ea8449640348d5e3c56d716224268bdd
kristina.kucerova <kiki.kucerova98@gmail.com>
2026-03-12 20:51:32 +0100

    ahoj

--- 07_pohyby_na_uctu.py
+++ 07_pohyby_na_uctu.py
@@ -1,16 +1,17 @@
-pohyby = [1200, -250, -800, 540, 721, -613, -222]
+# pohyby = [1200, -250, -800, 540, 721, -613, -222]
 
-print (pohyby[2])
-print (pohyby[2:])
+# print (pohyby[2])
+# print (pohyby[2:])
 
-len_pohyby = len(pohyby)
-print (len_pohyby)
+# len_pohyby = len(pohyby)
+# print (len_pohyby)
 
-max_pohyby = max(pohyby)
-print (max_pohyby)
+# max_pohyby = max(pohyby)
+# print (max_pohyby)
 
-min_pohyby = min(pohyby)
-print (min_pohyby)
+# min_pohyby = min(pohyby)
+# print (min_pohyby)
 
-sum_pohyby = sum(pohyby)
-print (sum_pohyby)
+# sum_pohyby = sum(pohyby)
+# print (sum_pohyby)
+print ("ahoj")
